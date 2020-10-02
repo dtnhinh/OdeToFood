9bd0afa814bad0f23f196f5ea1e87a9f4f07a174
Nhinh Dao <ndao@andculture.com>
2020-10-02 15:39:50 -0400

    Initial commit

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public IEnumerable<Restaurant> Restaurants;
+
+        public ListModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+            Restaurants = restaurantData.GetAll();
+        }
+    }
+}
